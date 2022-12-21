09b4062c3762fb00c4e3d180494810c35c3f8f4f
chandanch <chandan.cec107@gmail.com>
2022-12-21 19:15:23 +0530

    use of express validator :bulb:

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,27 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-	res.send(req.body);
-});
+router.post(
+	'/api/users/signup',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Invalid Email, enter a valid email ID'),
+		body('password')
+			.isLength({ min: 4 })
+			.withMessage('Password must have min of 4 characters'),
+	],
+	(req: Request, res: Response) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			res.status(400).send(errors.array());
+		}
+
+		res.status(201).send({ ...req.body, createdAt: new Date().toJSON() });
+	}
+);
 
 export { router as signupRouter };
