129f3b54f9452b7d14673a92be8bfa892ec780ab
chandanch <chandan.cec107@gmail.com>
2023-01-13 14:53:27 +0530

    create interfaces to handler usercreate and user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,27 @@
 import mongoose from 'mongoose';
 
+// Describes all properties required to an create user
+interface IUserCreate {
+	email: string;
+	password: string;
+}
+
+/**
+ * @description Describes all the properties and methods(instance or statics)
+ * that the User Mongoose Model must have
+ */
+interface IUserCreateModel extends mongoose.Model<IUser> {
+	build: (user: IUserCreate) => IUser;
+}
+
+/**
+ * @description Describes all the properties that the User Document must have
+ */
+interface IUser extends mongoose.Document {
+	email: string;
+	password: string;
+}
+
 const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -11,6 +33,15 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model<IUser, IUserCreateModel>('User', userSchema);
+
+userSchema.statics.build = (user: IUserCreate) => {
+	return new User(user);
+};
+
+const user = User.build({
+	email: 'ddd',
+	password: 'ddd',
+});
 
 export { User };
