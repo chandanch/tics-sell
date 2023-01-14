a3431472729827a51114c1bd4acb21605dc3a552
chandanch <chandan.cec107@gmail.com>
2023-01-14 22:47:47 +0530

    hash password using scrypt :bulb:

---
+++ auth/src/utils/password-hash.ts
@@ -0,0 +1,17 @@
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class PasswordHash {
+	static async generateHash(password: string): Promise<string> {
+		const salt = randomBytes(8).toString('hex');
+		const hash = (await scryptAsync(password, salt, 64)) as Buffer;
+
+		return `${hash.toString('hex')}.${salt}`;
+	}
+
+	static decodeHash(suppliedPassword: string, storedPassword: string) {}
+}
+
+PasswordHash.generateHash('dddd').then((data) => console.log(data));
