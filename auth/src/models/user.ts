6155d860468748e36791f533819ab8cada7258d0
chandanch <chandan.cec107@gmail.com>
2023-01-30 09:50:41 +0530

    transform document using toJSON and transform

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -23,16 +23,28 @@ interface IUser extends mongoose.Document {
 	password: string;
 }
 
-const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
 	},
-	password: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		toJSON: {
+			transform(doc, ret) {
+				delete ret.password;
+				delete ret.__v;
+				ret.id = ret._id;
+				delete ret._id;
+			},
+		},
+	}
+);
 
 userSchema.statics.build = (user: IUserCreate) => {
 	return new User(user);
