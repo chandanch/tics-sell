6a626b47bf6e48a88462c4129dc29743bb1dd178
chandanch <chandan.cec107@gmail.com>
2023-01-31 09:38:40 +0530

    add request validation for signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-	res.send(req.body);
-});
+router.post(
+	'/api/users/signin',
+	[
+		body('email').isEmail().withMessage('Invalid Email Address'),
+		body('password').trim().notEmpty().withMessage('Password is required'),
+	],
+	(req: Request, res: Response) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			throw new RequestValidationError(errors.array(), 400);
+		}
+
+		res.status(200).send('OK');
+	}
+);
 
 export { router as signinRouter };
