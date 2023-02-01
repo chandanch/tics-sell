3add5b41cf373e24450bd8b4cfc7be6300825ac1
chandanch <chandan.cec107@gmail.com>
2023-02-02 00:27:04 +0530

    setup request validator middleware

---
+++ auth/src/middlewares/request-validator.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const requestValidator = (
+	req: Request,
+	response: Response,
+	next: NextFunction
+) => {
+	// TODO: Add logic to validate requests
+};
