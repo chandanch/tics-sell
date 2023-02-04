1b2bbcddea8e72a4551e1097fc2feddd6f9af1de
chandanch <chandan.cec107@gmail.com>
2023-02-04 09:53:46 +0530

    bind current user details to req object

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,14 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-const currentUser = (req: Request, res: Response, next: NextFunction) => {
+interface UserPayload {
+	email: string;
+	id: string;
+}
+
+declare global {
+	namespace Express {
+		interface Request {
+			currentUser?: UserPayload;
+		}
+	}
+}
+
+export const currentUser = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	if (!req.session?.jwt) {
 		next();
 	}
 
 	try {
-		const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
-		//TODO: Add jwt payload to request object
+		const payload = jwt.verify(
+			req.session?.jwt,
+			process.env.JWT_KEY!
+		) as UserPayload;
+		// set currentUser to req object
+		req.currentUser = payload;
 	} catch (error) {
 		console.log(error);
 	}
