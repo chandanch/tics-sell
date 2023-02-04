b5b0ac089ec71c1c62aaa87fa4651638dfdaa2ae
chandanch <chandan.cec107@gmail.com>
2023-02-04 06:52:59 +0530

    create current user middleware

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,1 +1,16 @@
-// TODO implement middleware
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+const currentUser = (req: Request, res: Response, next: NextFunction) => {
+	if (!req.session?.jwt) {
+		next();
+	}
+
+	try {
+		const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+		//TODO: Add jwt payload to request object
+	} catch (error) {
+		console.log(error);
+	}
+	next();
+};
