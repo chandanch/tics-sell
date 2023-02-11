c58dbd0d42e4b7d50aa9d2ca3538a958a1a34aaa
chandanch <chandan.cec107@gmail.com>
2023-02-11 08:22:19 +0530

    add beforeall and afterAll hooks for mongo

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,1 +1,21 @@
-// TODO: Create setup for jests
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: any;
+beforeAll(async () => {
+	//instantiate MongoMemoryServer
+	mongo = new MongoMemoryServer();
+	const mongoUri = await mongo.getUri();
+
+	// connect mongoose to mongo mem server
+	await mongoose.connect(mongoUri, {});
+});
+
+afterAll(async () => {
+	if (mongo) {
+		// Stop Mongo DB & mem server
+		await mongo.stop();
+	}
+	// close connection
+	await mongoose.connection.close();
+});
