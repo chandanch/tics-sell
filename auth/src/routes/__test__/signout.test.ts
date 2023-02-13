418f1a16c9eb02c766311dd87c30885c021f7322
chandanch <chandan.cec107@gmail.com>
2023-02-13 08:07:32 +0530

    sign out tests :bulb:

---
+++ auth/src/routes/__test__/signout.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+const signinUrl = '/api/users/signin';
+const signupUrl = '/api/users/signup';
+const signoutUrl = '/api/users/signout';
+
+it('should remove session cookie on signout', async () => {
+	const response = await request(app)
+		.post(signupUrl)
+		.send({ email: 'dd@ee.com', password: 'password' });
+	expect(response.statusCode).toEqual(201);
+
+	const signoutResponse = await request(app).post(signoutUrl);
+	expect(signoutResponse.statusCode).toEqual(200);
+	expect(signoutResponse.get('Set-Cookie')).toBeDefined();
+});
