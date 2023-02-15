3492c33064d3d213d5dbf454b2225ee6bc5425e9
chandanch <chandan.cec107@gmail.com>
2023-02-15 09:32:40 +0530

    fix cookie issue in test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -5,12 +5,18 @@ const signupUrl = '/api/users/signup';
 const currentUserUrl = '/api/users/currentuser';
 
 it('should return user details', async () => {
-	const response = await request(app)
+	const authResponse = await request(app)
 		.post(signupUrl)
 		.send({ email: 'dd@ee.com', password: 'password' });
-	expect(response.statusCode).toEqual(201);
+	expect(authResponse.statusCode).toEqual(201);
 
-	const currentUserResponse = await request(app).get(currentUserUrl).send();
-	expect(currentUserResponse.statusCode).toEqual(401);
-	console.log(currentUserResponse);
+	const cookie = authResponse.get('Set-Cookie');
+	const currentUserResponse = await request(app)
+		.get(currentUserUrl)
+		.set('Cookie', cookie)
+		.send();
+	expect(currentUserResponse.statusCode).toEqual(200);
+	const { email, id } = currentUserResponse.body.currentUser;
+
+	expect(email).toEqual('dd@ee.com');
 });
