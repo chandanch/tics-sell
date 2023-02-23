ddca2f8b74038873dd7b95f0d47ef53570be7255
chandanch <chandan.cec107@gmail.com>
2023-02-23 08:23:01 +0530

    setup bootstrap lib and App component

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -4,6 +4,9 @@ const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard page</h1>
+      <button type="button" className="btn btn-primary">
+        Download
+      </button>
     </div>
   );
 };
