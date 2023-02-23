bb76ae6c12f3a4a3457324cf13661eb3e2ff02d7
chandanch <chandan.cec107@gmail.com>
2023-02-23 09:39:42 +0530

    signup form and submission :bulb:

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const SignUp = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		console.log(email, password);
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<h1>Sign Up</h1>
+			<div className="form-group">
+				<label>Email Address</label>
+				<input
+					type="text"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					className="form-control"
+				/>
+			</div>
+			<div className="form-group">
+				<label>Password</label>
+				<input
+					type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					className="form-control"
+				/>
+			</div>
+			<button className="btn btn-primary">Sign Up</button>
+		</form>
+	);
+};
