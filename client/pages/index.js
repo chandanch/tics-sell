564c548e8ec6d213c5820b06b90d0e8a57e92221
chandanch <chandan.cec107@gmail.com>
2023-03-02 00:16:58 +0530

    add nginx ingress url for ssr path :bulb:

--- client/pages/index.js
+++ client/pages/index.js
@@ -12,6 +12,14 @@ const HomePage = ({ currentUser }) => {
 
 HomePage.getInitialProps = async () => {
 	if (typeof window === undefined) {
+		const requestUrl =
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
+		const { data } = await axios.get(requestUrl, {
+			headers: {
+				Host: 'ticketing.dev',
+			},
+		});
+		return data;
 	} else {
 		const { data } = await axios.get('/api/users/currentuser');
 		return data;
