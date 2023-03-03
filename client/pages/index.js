2cfab8d07c45844e43ee9c0b08f6d261323ce2dc
chandanch <chandan.cec107@gmail.com>
2023-03-03 07:47:37 +0530

    bind headers from getInitialProps param :bulb:

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,17 +10,17 @@ const HomePage = ({ currentUser }) => {
 	);
 };
 
-HomePage.getInitialProps = async () => {
-	if (typeof window === undefined) {
+HomePage.getInitialProps = async ({ req }) => {
+	if (typeof window === 'undefined') {
+		console.log('SSR Making Request');
 		const requestUrl =
 			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
 		const { data } = await axios.get(requestUrl, {
-			headers: {
-				Host: 'ticketing.dev',
-			},
+			headers: req.headers,
 		});
 		return data;
 	} else {
+		console.log('On browser or client');
 		const { data } = await axios.get('/api/users/currentuser');
 		return data;
 	}
