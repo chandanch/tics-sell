41d070dc992ddfd135828d127d8dc58ae3648caa
chandanch <chandan.cec107@gmail.com>
2023-03-03 09:51:43 +0530

    handle error in current user req.

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,15 +6,24 @@ const HomePage = ({ currentUser }) => {
 	console.log(currentUser);
 	return (
 		<div>
-			<h1>HomePage</h1>
+			{currentUser ? (
+				<h1> Hello {currentUser.email} You are Signed In</h1>
+			) : (
+				<h1>You are not Signed In, Login to Access</h1>
+			)}
 		</div>
 	);
 };
 
 HomePage.getInitialProps = async (context) => {
 	const client = await createClient(context);
-	const { data } = await client.get('/api/users/currentuser');
-	return data;
+	try {
+		const { data } = await client.get('/api/users/currentuser');
+		return data;
+	} catch (error) {
+		console.log(error);
+		return {};
+	}
 };
 
 // export function getServerSideProps() {
