1b556dd0bef659834a1c3630101328f1da57ea4a
chandanch <chandan.cec107@gmail.com>
2023-03-03 20:03:20 +0530

    init header component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,13 @@
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from '../components/Header';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<div>
+			<Header />
+			<Component {...pageProps} />
+		</div>
+	);
 };
 
 export default MyApp;
