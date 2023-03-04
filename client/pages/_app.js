2e031b5cfa591ff749d6294a680b417f65e4d0b8
chandanch <chandan.cec107@gmail.com>
2023-03-04 10:18:18 +0530

    invoke page getinitialprops from wrapper component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Header from '../components/Header';
 import createClient from '../utils/create-api-client';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps, currentUser }) => {
 	return (
 		<div>
 			<Header />
@@ -15,7 +15,18 @@ MyApp.getInitialProps = async (appcontext) => {
 	const client = await createClient(appcontext.ctx);
 	try {
 		const { data } = await client.get('/api/users/currentuser');
-		return data;
+		let pageProps = {};
+
+		if (appcontext.Component.getInitialProps) {
+			pageProps = await appcontext.Component.getInitialProps(appcontext.ctx);
+		}
+
+		console.log('Page Props', pageProps);
+
+		return {
+			pageProps,
+			...data,
+		};
 	} catch (error) {
 		console.log(error);
 		return {};
