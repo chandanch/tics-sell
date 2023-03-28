f3d4de1e5d6f432f507725d95e19c08b902796fd
chandanch <chandan.cec107@gmail.com>
2023-03-28 09:34:34 +0530

    setup of shared lib using ts with tsc build

---
+++ shared/src/index.ts
@@ -0,0 +1,14 @@
+interface Player {
+  score: number;
+  name: string;
+}
+
+class Hockeer implements Player {
+  score: number;
+  name: string;
+
+  constructor(name: string, score: number) {
+    this.name = name;
+    this.score = score;
+  }
+}
