c47cdc0b98f59322288242911f5972a388dca60a
chandanch <chandan.cec107@gmail.com>
2023-05-07 11:20:22 +0530

    tests setup and draft test cases :bulb:

---
+++ tickets/src/routes/__test__/create-ticket.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('should create a ticket if valid details are provided', () => {});
+
+it('should return error if invalid price is provided', () => {});
+
+it('should return error if invalid title is provided', () => {});
