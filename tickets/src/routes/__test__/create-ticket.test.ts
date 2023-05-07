3fd4c7a6eafe489d49d035a94fa8db3680f0d19b
chandanch <chandan.cec107@gmail.com>
2023-05-07 18:46:49 +0530

    test to check for signed in user :bulb:

--- tickets/src/routes/__test__/create-ticket.test.ts
+++ tickets/src/routes/__test__/create-ticket.test.ts
@@ -1,9 +1,17 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+const tickets_url = '/api/tickets';
+
 it('should consists of a router hanlder for post requests', async () => {
-	const response = await request(app).post('/api/tickets').send({});
+	const response = await request(app).post(tickets_url).send({});
 	expect(response.statusCode).not.toEqual(404);
+});
+
+it('should be accessible only by signed in users', async () => {
+	const response = await request(app).post(tickets_url).send({});
+
+	expect(response.statusCode).toEqual(401);
 });
 
 it('should create a ticket if valid details are provided', () => {});
