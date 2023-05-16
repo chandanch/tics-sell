7d4b06029e4affa01adfd4da6caa281de728dd68
chandanch <chandan.cec107@gmail.com>
2023-05-16 14:20:15 +0530

    show ticket tests

--- tickets/src/routes/__test__/show-ticket.test.ts
+++ tickets/src/routes/__test__/show-ticket.test.ts
@@ -1,15 +1,31 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/tickets';
 
 const ticketsUrl = '/api/tickets';
 
 it('should return 404 for invalid ticket ID', async () => {
-	const response = await request('app')
-		.get(`${ticketsUrl}/2`)
+	const response = await request(app)
+		.get(`${ticketsUrl}/544545`)
 		.set('Cookie', global.signup())
 		.send();
 
 	expect(response.statusCode).toEqual(404);
 });
 
-it('should return ticket details for valid ticket ID', async () => {});
+it('should return ticket details for valid ticket ID', async () => {
+	// create the ticket
+	const mock_ticket_data = { title: 'A new Showman', price: 23, userId: '3' };
+	const ticket = Ticket.build(mock_ticket_data);
+	await ticket.save();
+
+	expect(ticket.id).toBeDefined();
+
+	const response = await request(app)
+		.get(`${ticketsUrl}/${ticket.id}`)
+		.set('Cookie', global.signup())
+		.send();
+
+	expect(response.statusCode).toEqual(200);
+	expect(response.body.title).toEqual(ticket.title);
+});
